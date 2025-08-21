11e4adeae42b90e2187ebadae7f7e0880796fc53
denismironov <mironov.d@valiotti.com>
2025-08-21 20:23:16 +0200

    fix footer.js

--- footer.js
+++ footer.js
@@ -26,6 +26,7 @@
       const checkOutseta = setInterval(() => {
         if (typeof Outseta !== 'undefined') {
           clearInterval(checkOutseta);
+          resolve();
         }
       }, 100);
 
